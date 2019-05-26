7af6edd8ead0d2b31eb1fdd8447ff34ff2a762ba
Alexey <kalinichenkoae@gmail.com>
2019-05-26 10:36:32 +0300

    edit settings menu

---
+++ Assets/Scripts/SliderUI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderUI : MonoBehaviour
+{
+    public GameObject mMusicOn;
+    public GameObject mMusicOff;
+    public GameObject mSoundOn;
+    public GameObject mSoundOff;
+    
+    void Start()
+    {
+        mMusicOn.SetActive(false);
+        mMusicOff.SetActive(true);
+        mSoundOn.SetActive(false);
+        mSoundOff.SetActive(true);
+    }
+
+    public void OnMusicValueChange(float slider)
+    {
+        if (slider == 0)
+        {
+            mMusicOn.SetActive(false);
+            mMusicOff.SetActive(true);
+        }
+        else
+        {
+            mMusicOn.SetActive(true);
+            mMusicOff.SetActive(false);
+        }
+    }
+    
+    public void OnSoundValueChange(float slider)
+    {
+        if (slider == 0)
+        {
+            mSoundOn.SetActive(false);
+            mSoundOff.SetActive(true);
+        }
+        else
+        {
+            mSoundOn.SetActive(true);
+            mSoundOff.SetActive(false);
+        }
+    }
+}
