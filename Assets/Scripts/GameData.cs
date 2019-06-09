5059eed888d88ebc845e5f0c2355672292a37caa
Alexey <kalinichenkoae@gmail.com>
2019-06-09 22:46:18 +0300

    added settings and texts configs

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Xml.Linq;
+
+public class GameData : MonoBehaviour
+{
+    private Settings mSettings;
+    private Dictionary<string, string> mTextsRu;
+    private Dictionary<string, string> mTextsEn;
+
+    void Start()
+    {
+        mTextsRu = new Dictionary<string, string>();
+        mTextsEn = new Dictionary<string, string>();
+        mSettings = new Settings();
+        
+        LoadSettings();
+        LoadTexts();
+    }
+
+    void OnApplicationQuit()
+    {
+        UnloadSettings();
+    }
+    
+    private string GetConfigData(string fileName)
+    {
+        var configPath = Path.Combine(Application.streamingAssetsPath + "/", fileName);
+        string data;
+            
+#if UNITY_EDITOR || UNITY_IOS
+        data = File.ReadAllText(configPath);
+#elif UNITY_ANDROID
+        WWW reader = new WWW (configPath);
+        while (!reader.isDone) {}
+        data = reader.text;
+#endif
+        return data;
+    }
+
+    private void LoadSettings()
+    {
+        var configData = GetConfigData("settings.json");
+        
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream);
+        var serializer = new DataContractJsonSerializer(typeof(Settings));
+        
+        writer.Write(configData);
+        writer.Flush();
+        stream.Position = 0;
+        
+        mSettings = (Settings)serializer.ReadObject(stream);
+        
+        if (mSettings == null)
+            mSettings = new Settings();
+    }
+
+    private void UnloadSettings()
+    {
+        var stream = new MemoryStream();
+        var serializer = new DataContractJsonSerializer(typeof(Settings));
+        
+        serializer.WriteObject(stream, mSettings);
+        
+        var data = System.Text.Encoding.UTF8.GetString(stream.ToArray());
+        var configPath = Path.Combine(Application.streamingAssetsPath + "/", "settings.json");
+
+        File.WriteAllText(configPath, data);
+    }
+    
+    private void LoadTexts()
+    {
+        var configData = GetConfigData("texts_en.xml");
+
+        var texts = XElement.Parse(configData);
+        var text = texts.FirstNode;
+
+        while (text != null)
+        {
+            var textNode = (XElement) text;
+            var textKey = textNode.Name.ToString();
+            var textValue = textNode.FirstAttribute.Value;
+
+            mTextsEn.Add(textKey, textValue);
+            
+            text = text.NextNode;
+        }
+        
+        configData = GetConfigData("texts_ru.xml");
+
+        texts = XElement.Parse(configData);
+        text = texts.FirstNode;
+        
+        while (text != null)
+        {
+            var textNode = (XElement) text;
+            var textKey = textNode.Name.ToString();
+            var textValue = textNode.FirstAttribute.Value;
+
+            mTextsRu.Add(textKey, textValue);
+            
+            text = text.NextNode;
+        }
+    }
+
+    public string GetString(string key)
+    {
+        var texts = mTextsEn;
+
+        if (mSettings.GetLanguage() == Settings.eLanguage.RUSSIAN)
+            texts = mTextsRu;
+
+        return texts[key];
+    }
+
+    public float GetMusicVolume() { return mSettings.GetMusicVolume(); }
+    public float GetSoundVolume() { return mSettings.GetSoundVolume(); }
+    public Settings.eLanguage GetLanguage() { return mSettings.GetLanguage(); }
+
+    public void SetMusicVolume(float volume) { mSettings.SetMusicVolume(volume); }
+    public void SetSoundVolume(float volume) { mSettings.SetSoundVolume(volume); }
+    public void SetLanguage(Settings.eLanguage language) { mSettings.SetLanguage(language); }
+}
+
+[DataContract]
+public class Settings
+{
+    public enum eLanguage { RUSSIAN, ENGLISH };
+
+    [DataMember] private string Language = "en";
+    [DataMember] private string MusicVolume = "0.5";
+    [DataMember] private string SoundVolume = "0.5";
+
+    public eLanguage GetLanguage()
+    {
+        var lang = eLanguage.ENGLISH;
+        
+        if (Language == "ru")
+            return eLanguage.RUSSIAN;
+
+        return lang;
+    }
+
+    public float GetMusicVolume()
+    {
+        return float.Parse(MusicVolume, CultureInfo.InvariantCulture.NumberFormat);
+    }
+    
+    public float GetSoundVolume()
+    {
+        return float.Parse(SoundVolume, CultureInfo.InvariantCulture.NumberFormat);
+    }
+
+    public void SetLanguage(eLanguage language)
+    {
+        Language = (language == eLanguage.RUSSIAN) ? "ru" : "en";
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        MusicVolume = volume.ToString();
+    }
+    
+    public void SetSoundVolume(float volume)
+    {
+        SoundVolume = volume.ToString();
+    }
+}
--- Assets/Scripts/SliderUI.cs
+++ Assets/Scripts/SliderUI.cs
@@ -8,13 +8,27 @@ public class SliderUI : MonoBehaviour
     public GameObject mMusicOff;
     public GameObject mSoundOn;
     public GameObject mSoundOff;
-    
+
     void Start()
     {
         mMusicOn.SetActive(false);
         mMusicOff.SetActive(true);
         mSoundOn.SetActive(false);
         mSoundOff.SetActive(true);
+
+        GameObject.Find("slider_music").GetComponent<Slider>().value =
+            GameObject.Find("Main Camera").GetComponent<GameData>().GetMusicVolume();
+        
+        GameObject.Find("slider_sound").GetComponent<Slider>().value =
+            GameObject.Find("Main Camera").GetComponent<GameData>().GetSoundVolume();
+
+        var lang = GameObject.Find("Main Camera").GetComponent<GameData>().GetLanguage();
+        var langNumber = 0;
+
+        if (lang == Settings.eLanguage.RUSSIAN)
+            langNumber = 1;
+
+        GameObject.Find("dropdown_lang").GetComponent<Dropdown>().value = langNumber;
     }
 
     public void OnMusicValueChange(float slider)
@@ -29,6 +43,8 @@ public class SliderUI : MonoBehaviour
             mMusicOn.SetActive(true);
             mMusicOff.SetActive(false);
         }
+
+        GameObject.Find("Main Camera").GetComponent<GameData>().SetMusicVolume(slider);
     }
     
     public void OnSoundValueChange(float slider)
@@ -43,5 +59,17 @@ public class SliderUI : MonoBehaviour
             mSoundOn.SetActive(true);
             mSoundOff.SetActive(false);
         }
+        
+        GameObject.Find("Main Camera").GetComponent<GameData>().SetSoundVolume(slider);
+    }
+
+    public void OnLanguageChange()
+    {
+        int lang = GameObject.Find("dropdown_lang").GetComponent<Dropdown>().value;
+        
+        if (lang == 1)
+            GameObject.Find("Main Camera").GetComponent<GameData>().SetLanguage(Settings.eLanguage.RUSSIAN);
+        else
+            GameObject.Find("Main Camera").GetComponent<GameData>().SetLanguage(Settings.eLanguage.ENGLISH);
     }
 }
